b994af1bba9d616725014f41a8c362d246e68ef2
Asima Abid <asimaabid124@gmail.com>
2020-06-14 17:46:24 +0500

    assignment js

---
+++ app.js
@@ -0,0 +1,594 @@
+//---------CHAPTER#1 Alerts--------------
+
+// --------Chapter :1 Task:1 -----------
+// alert("Hello Welcome to my website");
+
+// --------Chapter :1 Task:2 ------------
+// alert("Error!Please enter a valid password");
+
+// --------Chapter :1 Task:3 -----------
+// alert("Welcome to JS land....\n Happy Coding");
+
+// --------Chapter :1 Task:4 -----------
+// alert("Welcome to JS land....");
+// alert("Prevent this page from creating additional dialogs");
+
+// --------Chapter :1 Task:5 -----------
+// alert("Hello... i can run JS through my browser's console");
+// console.log(alert("Hello... i can run JS through my browser's console"));
+
+// --------Chapter :1 Task:6 -----------
+// alert("I am showig alert in my webiste");
+
+// --------Chapter :1 Task:7 -----------
+// var a=10;
+// var b=10;
+// var c=a+b;
+// console.log(c);
+
+//---------CHAPTER#2 Variables for strings--------------
+
+// --------Chapter :2 Task:1 -----------
+// var username;
+
+// --------Chapter :2 Task:2 -----------
+// var myname="Asima Abid";
+
+// --------Chapter :2 Task:3 -----------
+//3 (a)
+// var message;
+//3 (b)
+// message="hello world";
+//3 (c)
+// alert(message);
+
+// --------Chapter :2 Task:4 -----------
+// var name="John Doe";
+// var age="15 years old";
+// var course="certified mobile application development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+// --------Chapter :2 Task:5 -----------
+// var x=("pizza\npizz \npiz \npi  \np");
+// alert(x);
+
+// --------Chapter :2 Task:6 -----------
+// var email="asima@gmail.com";
+// alert("my email is "+email);
+
+// --------Chapter :2 Task:7 -----------
+// var book="A smarter way to learn JavaScript ";
+// alert("i am trying to learn from the book "+book);
+
+// --------Chapter :2 Task:8 -----------
+// document.write("Yah! I can wrire HTML content through javascript");
+
+// --------Chapter :2 Task:9 -----------
+
+
+//---------CHAPTER#3 Variables for numbers--------------
+// --------Chapter :3 Task:1 -----------
+// var age=19;
+// alert("your age is "+age+"year old");
+
+// --------Chapter :3 Task:2 -----------
+// var n=12;
+// alert("You have visited this website "+n+" times");
+
+// --------Chapter :3 Task:3 -----------
+// var birthyear="2001";
+// alert("Your birth year is "+birthyear+"\n data type of your decleared variable is number");
+//  document.write("Asima");
+
+// --------Chapter :3 Task:4 -----------
+// var visitor_name= "Asima";
+// var pr_title= "Lawn Shirt";
+// var quantity= "2";
+// alert(visitor_name+" ordered "+quantity+" "+pr_title+" on Shpping Palace Clothing store");
+
+//---------CHAPTER#4 Variable names: Legal & illegal--------------
+// --------Chapter :4 Task:1 -----------
+// var a=1, b=2, c=3;
+
+// --------Chapter :4 Task:2 -----------
+//  var _name,age1,myAlert,twoVar,time;
+//  var #name,age^,my*age,var,alert;
+
+// --------Chapter :4 Task:3 -----------
+//  document.write("Variable names can only contain number,$ and _ For example $my_1stVariable <br> Variables must begin with a numer,letter or _ <br>For example $name, _name or name <br>  Variable names are case senstitive <br>Variable names should not be JS keyword");
+
+
+//---------CHAPTER#5 Math expression--------------
+
+// --------Chapter :5 Task:1 ----------
+//    var num1= +prompt("Enter a number");
+//    var num2= +prompt("Enter number");
+//    num3=num1+num2;
+// document.write("Sum of "+num1+" and "+num2+" is "+num3 + "<br>");
+
+// --------Chapter :5 Task:2 ----------
+// num4=num1-num2;
+// document.write("Subtraction of "+num1+" and "+num2+" is "+num4+ "<br>");
+// num5=num1/num2;
+// document.write("Division of "+num1+" and "+num2+" is "+num5 + "<br>");
+// num6=num1%num2;
+// document.write("Modulus of "+num1+" and "+num2+" is "+num6 + "<br>");
+
+// --------Chapter :5 Task:3 ----------
+// var a;
+// document.write("Value after declaration is:"+a +"\n a");
+// var a=10;
+// document.write("initial value:"+a);
+// a++
+// document.write("After increament:"+a);
+// a=a+7;
+// document.write("Value after addition is:"+a);
+// a--
+// document.write("Value after decreament is:"+a);
+// a=a/3
+// document.write(" Value after reaminder is:"+a);
+
+// --------Chapter :5 Task:4 ----------
+// var ticketPrice=600;
+// totalCost=ticketPrice*5;
+// document.write("Total Cost to buy 5 tickets to a movie is "+totalCost+"PKR");
+
+// --------Chapter :5 Task:5 ----------
+// document.write("Table of 4 <br>")
+// for(a=1;a<10;a++)
+// {
+//    document.write("4*"+a+"="+a*4);
+//    document.write("<br>");
+// }
+
+// --------Chapter :5 Task:6 ----------
+// var celciusTemp=70;
+//  fer_temp=(celciusTemp-32)*((5/9));
+// var ferTemp=25;
+// cel_Temperature=(ferTemp*(9/5))+32;
+//  document.write(celciusTemp+"is "+fer_temp);
+//  document.write("<br>");
+// document.write(ferTemp+" is "+cel_Temperature);
+
+// --------Chapter :5 Task:7 ----------
+// var pr_item1=400;
+// var pr_item2=700;
+// var quantity_item1=2;
+// var quantity_item2=4;
+// var Shipping=150;
+// var cost=((pr_item1*quantity_item1)+(pr_item2*quantity_item2)+Shipping);
+// document.write("Price of item 1 is "+pr_item1+"<br>");
+// document.write("Price of item 2 is "+pr_item2+"<br>");
+// document.write("Quantity of item 1 is "+quantity_item1+"<br>");
+// document.write("Quantity of item 2 is "+quantity_item2+"<br>");
+// document.write("Shipping Charges is "+Shipping+"<br>");
+// document.write("Total cost of your order is "+cost+"<br>");
+
+// // --------Chapter :5 Task:8 ----------
+// var total=500;
+// var marks_obtained=460;
+//  var per=((marks_obtained)*100)/total;
+//  document.write("Mark Sheet"+"<br>");
+//  document.write("Total marks "+total+"<br>");
+// document.write("Marks Obtained is "+marks_obtained+"<br>");
+// document.write("Percentage is "+per+"%");
+
+ // --------Chapter :5 Task:9 ----------
+//  var dollar=10;
+//  var SaudiRiyal=25;
+//  var ex=(dollar*104.80)+(SaudiRiyal*28);
+//  document.write("Total currency in PKR is :"+ex);
+
+// --------Chapter :5 Task:10 ----------
+// var a=20;
+// document.write(((a+5)*20)/2);
+
+// --------Chapter :5 Task:11 ----------
+// var currentYear=2020;
+// var birthyear=2001;
+// var age=currentYear-birthyear;
+// document.write("Current year: "+currentYear+"<br>");
+// document.write("Birth year: "+birthyear+"<br>");
+// document.write("Your age is : "+age);
+
+// --------Chapter :5 Task:12 ----------
+// var r=20;
+// var circumference= 2*(3.142)*r;
+//  var area=(3.142)*(r*r);
+// document.write("Radius Of Circle is : "+r+"<br>");
+// document.write("The circumference Of Circle is : "+circumference+"<br>");
+// document.write("Area Of Circle is : "+area+"<br>");
+
+//---------CHAPTER#6-9 Math expression--------------
+// --------Chapter :6-9 Task:1 ----------
+// var a=10;
+// document.write(a)
+// document.write("<br>")
+// ++a;
+// document.write("The value of ++a is :"+a );
+// document.write("<br>")
+// document.write("Now the value of a is :"+a);
+// document.write("<br>")
+// a++;
+// document.write("The value of a++ is :"+a );
+// document.write("<br>")
+// --a;
+// document.write("Now the value of a is :"+a);
+// document.write("<br>")
+// a--;
+// document.write("The value of a-- is :"+a );
+// document.write("<br>");
+// document.write("Now the value of a is :"+a);
+
+// --------Chapter :6-9 Task:2 ----------
+// var a=2;
+// var b=1;
+// result= --a - --b + ++b +b--;
+// document.write(" a is "+a+"<br>");
+// document.write(" b is "+b+"<br>");
+// document.write(" Result is "+result+"<br>");
+
+// --------Chapter :6-9 Task:3 ----------
+// var x=prompt("Enter your name");
+// alert("Welcome to this page "+x);
+
+// --------Chapter :6-9 Task:5 ----------
+// var table= prompt("Enter a number");
+// if (table>0) {
+//     for(a=1;a<=10;a++)
+// {
+//    document.write(table+"*"+a+"="+table*a);
+//     document.write("<br>");
+//  }
+// }
+// else {
+//  for(b=1;b<=10;b++)
+//  {
+//     document.write("5*"+b+"="+5*b);
+//     document.write("<br>");
+//  }
+// }
+
+// --------Chapter :6-9 Task:6 ----------
+// var sub1=prompt("Enter name of first subject");
+// var sub2=prompt("Enter name of second subject");
+// var sub3=prompt("Enter name of third subject");
+// var total=100;
+// var nums1=prompt("enter the number of "+sub1);
+// var nums2=prompt("enter the number of "+sub2);
+// var nums3=prompt("enter the number of "+sub3);
+// document.write(sub1+total+num1+per);
+
+//---------CHAPTER#9-11 USER INPUT & CONDITIONAL STATEMENT--------------
+
+// --------Chapter :9-11 Task:1 ----------
+// var a=prompt("Enter city name:");
+// if (a=="Karachi")
+// {
+//   alert("Welcome to city of lights");
+// }
+
+// --------Chapter :9-11 Task:2 ----------
+// var gender=prompt("Enter your gender please");
+// if(gender=="male")
+// {
+//  document.write("Good morning Sir");
+
+// }
+// else
+// {
+//    document.write("Good morning Ma'am");
+   
+// }
+
+// --------Chapter :9-11 Task:3 ----------
+// var color=prompt("Please enter the signal color");
+// if(color=="Red"){
+//    document.write("\t Must Stop");
+// }
+// else if(color=="Yellow"){
+//    document.write("Ready to move");
+// }
+// else
+// {
+//    document.write("Move now");
+// }
+
+// --------Chapter :9-11 Task:4 ----------
+// var fuel=prompt("Enter the remaining fuel in car(in litre)")
+// if(fuel<0.25)
+// {
+//    alert("Please refill fuel in your car");
+// }
+
+// --------Chapter :9-11 Task:5 ----------
+// This code is  running, there is no error, because there is pre-increament in variable
+
+// --------Chapter :9-11 Task:6 ----------
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// --------Chapter :9-11 Task:7 ----------
+//  var sub1=prompt("Enter name of first subject");
+//  var sub2=prompt("Enter name of second subject");
+//  var sub3=prompt("Enter name of third subject");
+// //  var max_total=100;
+//  var sub_total=sub1+sub2+sub3;
+//  var per=(sub_total*100)/300;
+ 
+
+//  if(per>=80){
+//    document.write("Total marks:300");
+// //    document.write("<br>");
+// //   document.write(var sub_total);
+// //     document.write("A-One");
+
+
+//  }
+//  else if(per>=70){
+//    document.write("Total marks:300");
+// //    document.write("<br>");
+// //   document.write(var sub_total);
+//    document.write("Good");
+
+// }
+// else if(per>=60){
+    //    document.write("Total marks:300");
+// //    document.write("<br>");
+// //   document.write(var sub_total);
+
+//    document.write("You need to improve");
+
+// }
+// else
+// {
+//    document.write("Total marks:300");
+// //    document.write("<br>");
+// //   document.write(var sub_total);
+//    document.write("Sorry");
+
+// }
+
+
+// --------Chapter :9-11 Task:7 ----------
+// var number=prompt("Enter a number between 1 and 100");
+// var S_number=20;
+// num2=num1+1;
+//   if(number==S_number){
+//   document.write("Bingo.... Correct number");
+//   }
+  
+//   else if(num2==S_number){
+//    document.write("Close enough to corect number");
+//    }
+//    else{
+//       document.write("wrong");
+//    }
+
+// --------Chapter :9-11 Task:8 ----------
+// var divNum=prompt("Enter a number");
+// if ((divNum%3)==0){
+//    alert("Number is divisible by 3");
+// }
+
+// --------Chapter :9-11 Task:9 ----------
+// var num= +prompt("Enter the  number");
+// if((num%2)==0){
+//     alert("its even number");
+// }
+// else{
+//     alert("its odd number");
+// }
+
+// --------Chapter :9-11 Task:11 ----------
+// var num1= +prompt("Enter the first number");
+// var num2= +prompt("Enter the second number");
+// var op=prompt("Enter a operator");
+// sum=num1+num2;
+// dif=num1-num2;
+// mul=num1*num2;
+// if(op=="+"){
+//    alert("Sum is "+sum);
+// }
+// else  if(op=="-"){
+//    alert("Difference is "+diff);
+// }
+// else  if(op=="*"){
+//    alert("Multiplication is "+mul);
+// }
+
+
+// --------Chapter :12-13 IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS ----------
+// --------Chapter :12-13 Task:1 ----------
+// var character=prompt("Please enter a character");
+// if(character>=65 && character<=90){
+//    alert("UpperCase");
+// }
+
+// --------Chapter :12-13 Task:2 ----------
+// var fNumber= +prompt("Enter the first number");
+// var sNumber= +prompt("Enter the second number");
+// if(fNumber>sNumber){
+//    alert("Number 1 is greater");
+
+// }
+// else if(sNumber>fNumber){
+//    alert("Number 2 is greater");
+   
+// }
+// else{
+//    alert("Both are equal");
+// }
+
+// --------Chapter :12-13 Task:3 ----------
+// var number= +prompt("Enter the number");
+// if(number>"0"){
+//    alert("Number is possitive");
+
+// }
+// else if(number<"0"){
+//    alert("Number is negative");
+   
+// }
+// else{
+//    alert("Number is zero");
+// }
+
+// --------Chapter :12-13 Task:4----------
+// var ch= prompt("Enter a character");
+// if(ch=="a" || ch=="e"|| ch=="i"|| ch=="0"|| ch=="u")
+// {
+//    alert("True.. its a vowel");
+// }
+// else("False it is not a vowel");
+
+// --------Chapter :12-13 Task:5 ----------
+// var password=prompt("Enter you password please");
+// if (password==0)
+// {
+//    alert("Please enter your password");
+// }
+// if (password=="Asima")
+// {
+//    alert("Correct! The password you entered matches with original");
+// }
+// else
+// {
+//    alert("Incorrect! The password you entered does not matches with original");
+// }
+
+// --------Chapter :12-13 Task:6 ----------
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else
+// {
+// greeting = "Good evening";
+// }
+
+// --------Chapter :12-13 Task:7 ----------
+// var time=prompt("Enter the time:");
+// if(time==13){
+//    alert("1 PM")
+// }
+// else if(time==14){
+//    alert("2 PM")
+// }
+// else if(time==15){
+//    alert("3 PM")
+// }
+// else if(time==16){
+//    alert("4 PM")
+// }
+// else if(time==17){
+//    alert("5 PM")
+// }
+// else if(time==18){
+//    alert("6 PM")
+// }
+// else if(time==19){
+//    alert("7 PM")
+// }
+// else if(time==20){
+//    alert("8 PM")
+// }
+// else if(time==21){
+//    alert("9 PM")
+// }
+// else if(time==22){
+//    alert("10 PM")
+// }
+// else if(time==23){
+//    alert("11 PM")
+// }
+// else if(time==24){
+//    alert("12 PM")
+// }
+
+
+// --------Chapter :14-16 Array ----------
+
+// --------Chapter :14-16 Task:1 ----------
+//  var studentName=[];
+
+// --------Chapter :14-16 Task:2 ----------
+//  var studentName=[];
+
+// --------Chapter :14-16 Task:3 ----------
+//  var stringArray=['A','B','C'];
+
+// --------Chapter :14-16 Task:4 ----------
+//  var numArray=[1,2,3,4,5,6];
+
+// --------Chapter :14-16 Task:5 ----------
+//  var boolArray=[true,false];
+
+// --------Chapter :14-16 Task:6 ----------
+// var mix=["a", a, 1, true];
+
+// --------Chapter :14-16 Task:7 ----------
+// var education=["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+// document.write("Qualification");
+// document.write("<br>")
+// for (i=1;i<=7;i++){
+//     document.write(i+")    "+education[i]);
+//     document.write("<br>");
+// }
+
+// --------Chapter :14-16 Task:8 ----------
+// var st_name=["Asima","Amna","Arifa"];
+// var st_score=['345','456','400'];
+// for (i=0;i<3;i++)
+// {
+
+//  document.write("Score of "+st_name[i]+" is "+ st_score[i]+ "  Percentage is :" +st_score[i]/500*100 + "%");
+//  document.write("<br>")
+// }
+
+// --------Chapter :14-16 Task:9 ----------
+// array color=["Red","Green","Blue","Black","Grey","Yellow"];
+// var color=["Blue","Red","Green"];
+// var add=prompt("Which color yo")
+
+// --------Chapter :14-16 Task:10 ----------
+// var score=[320,234,567,890,333];
+// document(score.sort(a,b);
+
+// --------Chapter :14-16 Task:11 ----------
+// var city=['karachi','Attok','Peshawar','Rawalpindi','Hyderabad'];
+// var z=city.copyWithin(3,5);
+// document.write(z);
+
+// --------Chapter :14-16 Task:12 ----------
+// var arr=["This","is","my","pen"];
+// document.write("Array<br>");
+// document.write(arr+"<br>");
+// document.write("string"+"<br>");
+// document.write(arr.join(" "));
+
+// --------Chapter :14-16 Task:13 ----------
+// var devices=['keyborad','mouse','printer','monitor'];
+// document.write("Devices :<br>"+devices+"<br>");
+// for(i=0;i<4;i++){
+//     document.write("Out: <br>"+devices[i]+"<br>");
+// }
+// --------Chapter :14-16 Task:14 ----------
+// document.write("Devices :<br>"+devices+"<br>");
+// for(i=3;i>=0;i--){
+//     document.write("Out: <br>"+devices[i]+"<br>");
+// }
+
+// --------Chapter :14-16 Task:15 ----------
+// var phone=['Apple','Samsung','Motorolla','Nokia',''];
+
+
+
+
+
+
